1e059cafdfba30e8abdc8667ce543c5a4e329170
Gustavo <ghustavo516@gmail.com>
2024-11-26 22:11:16 -0300

    CHANGELOG: Ajuste na documentação da API

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ from Moneta.model.stocks_model import StocksModel
 from Moneta.scrapper.stocks_scrapper import MarketInfoExtractor
 from Moneta.scrapper.real_estate_scrapper import RealEstateScrapper
 
-app = FastAPI()
+app = FastAPI(title="Moneta",
+    description="API para consulta de dados do mercado financeiro",
+    version="1.0.0")
+
 @app.post("/stocks/by-name")
 async def get_info_finance_market(model: StocksModel):
     finance = MarketInfoExtractor()
@@ -23,6 +26,15 @@ async def get_info_real_estate(model:RealEstateModel):
     fiis = RealEstateScrapper()
     info_finance = fiis.get_real_estate_data(model.name_ticket)
     return info_finance
+
+
+# [] Gerar o requeriments
+# [] Documentar a api e gerar o swagger
+# [] Finalizar read.me
+
+# Desenvolver o intergrador para integrar todas as outras API para gerar o projeto completo
+
+
 
 
 
